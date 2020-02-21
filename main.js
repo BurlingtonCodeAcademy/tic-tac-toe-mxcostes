3a315a87d9fb45c3434d22fbe0567ef991d94b87
Max-Costes <mxcostes@gmail.com>
2020-02-21 15:09:35 -0500

    win check working

--- main.js
+++ main.js
@@ -9,13 +9,6 @@ let cell6 = document.getElementById('cell-6');
 let cell7 = document.getElementById('cell-7');
 let cell8 = document.getElementById('cell-8');
 let clock = document.getElementById('time');
-//may be useless
-// let row1 = document.getElementsByClassName('r1');
-// let row2 = document.getElementsByClassName('r2');
-// let row3 = document.getElementsByClassName('r3');
-// let column1 = document.getElementsByClassName('c1');
-// let c1Array = document.querySelectorAll('.c1')
-// ^^
 let startButton = document.getElementById('start');
 let resetButton = document.getElementById('reset');
 let playerTurn = document.getElementById('playerStatus');
@@ -66,47 +59,21 @@ function mark(event) {
 	if (event.target.textContent === '') {
 		if (gameOn === true && move % 2 === 0) {
 			event.target.textContent = 'x';
-			move = move + 1;
-			//winCheck()
-			for (let element of Object.values(winningCombos)) {
-				console.log(element[0].textContent);
-				console.log(element[1].textContent);
-				console.log(element[2].textContent);
-				if (element[0].textContent !== '') {
-					playerTurn.textContent = "Player O's turn";
-					console.log('not in the win side x');
-				} else if (
-					element[0].textContent === element[1].textContent &&
-					element[0] === element[2].textContent &&
-					element[0].textContent === 'x'
-				)
-					console.log('hit in the click');
+            move = move + 1;
+            playerTurn.textContent = "Player O's turn"
+            winCheck(event.target.textContent)
 				//winnerwinner()
-			}
-		} else if (gameOn === true && (move + 2) % 2 === 1) {
+			}  else if (gameOn === true && (move + 2) % 2 === 1) {
 			event.target.textContent = 'o';
-			move = move + 1;
-			//winCheck()
-			for (let element of Object.values(winningCombos)) {
-				console.log(element[0].textContent);
-				console.log(element[1].textContent);
-				console.log(element[2].textContent);
-				if (element[0].textContent === '') {
-					playerTurn.textContent = "Player X's turn";
-					console.log('not in the win side o');
-				} else if (
-					element[0].textContent === element[1].textContent &&
-					element[0] === element[2].textContent &&
-					element[0].textContent === 'o'
-				)
-					console.log('hit in the click');
-				//winnerwinner()
-			}
-		}
-	} else if (event.target.textContent === 'x' || event.target.textContent === 'o') {
+            move = move + 1;
+            playerTurn.textContent = "Player X's turn";
+                winCheck(event.target.textContent)
+        }
+
+	 else if (event.target.textContent === 'x' || event.target.textContent === 'o') {
 		playerTurn.textContent = 'Please select an empty cell';
-	}
-}
+	}}}
+
 
 //reset
 function reset() {
@@ -145,27 +112,38 @@ function timer() {
 }
 
 //winlogic
-// function checkForWin(){
-function winCheck() {
-	for (let element of c1Array) {
-		console.log(element.textContent.includes('x'));
-		if (element.textContent.includes('x')) {
-			winnerwinner();
-		}
-	}
-}
-//
-//win funciton
-function winnerwinner() {
-	console.log('hit in the funciton');
-	playerTurn.textContent = 'Congratulations You have won the game ';
-	// cell0.removeEventListener('click', mark);
-	// cell1.removeEventListener('click', mark);
-	// cell2.removeEventListener('click', mark);
-	// cell3.removeEventListener('click', mark);
-	// cell4.removeEventListener('click', mark);
-	// cell5.removeEventListener('click', mark);
-	// cell6.removeEventListener('click', mark);
-	// cell7.removeEventListener('click', mark);
-	// cell8.removeEventListener('click', mark);
+			
+        function winCheck(target){
+            for (let element of Object.values(winningCombos)) {
+                console.log(target)
+                console.log(element[0].textContent)
+				if (element[0].textContent === '') {
+                console.log('what is going on')
+					
+				} else if (
+					element[0].textContent === element[1].textContent &&
+					element[0].textContent === element[2].textContent && element[0].textContent===target 
+				){
+                    console.log(target + 'has won');
+                   for (cell of element){
+                       cell.className += 'winner'
+                   } 	
+				winnerwinner(target)
+			
+                }}}
+
+
+function winnerwinner(winner) {
+	
+    playerTurn.textContent = 'Congratulations ' + winner +' won the game ';
+    clearInterval(interval)
+	cell0.removeEventListener('click', mark);
+	cell1.removeEventListener('click', mark);
+	cell2.removeEventListener('click', mark);
+	cell3.removeEventListener('click', mark);
+	cell4.removeEventListener('click', mark);
+	cell5.removeEventListener('click', mark);
+	cell6.removeEventListener('click', mark);
+	cell7.removeEventListener('click', mark);
+	cell8.removeEventListener('click', mark);
 }
